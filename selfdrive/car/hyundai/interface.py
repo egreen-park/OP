3e9786713e20e0de55ad16d2a6a873278d2283fd
egreen-park <egreen_park@naver.com>
2022-01-22 08:43:24 +0900

    long update

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -25,7 +25,7 @@ class CarInterface(CarInterfaceBase):
     v_current_kph = current_speed * CV.MS_TO_KPH
 
     gas_max_bp = [0., 10., 20., 50., 70., 130.]
-    gas_max_v = [CarControllerParams.ACCEL_MAX, 1.4, 0.85, 0.72, 0.47, 0.22]
+    gas_max_v = [CarControllerParams.ACCEL_MAX, 1.3, 0.85, 0.72, 0.47, 0.22]
 
     return CarControllerParams.ACCEL_MIN, interp(v_current_kph, gas_max_bp, gas_max_v)
 
@@ -65,7 +65,7 @@ class CarInterface(CarInterfaceBase):
 
     # longitudinal
     ret.longitudinalTuning.kpBP = [0., 5.*CV.KPH_TO_MS, 10.*CV.KPH_TO_MS, 20.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.4, 1.14, 0.9, 0.78, 0.48] # 1.6, 1.18, 0.9, 0.78, 0.48
+    ret.longitudinalTuning.kpV = [1.4, 1.13, 0.9, 0.78, 0.48] # 1.6, 1.18, 0.9, 0.78, 0.48
     ret.longitudinalTuning.kiBP = [0., 130. * CV.KPH_TO_MS]
     ret.longitudinalTuning.kiV = [0.1, 0.06]
 
